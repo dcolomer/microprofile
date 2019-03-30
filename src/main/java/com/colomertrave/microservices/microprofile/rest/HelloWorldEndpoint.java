359e559688f29fb0e668f03eaf1adc72743ac1b6
daniel <colomertrave@gmail.com>
2019-03-30 20:04:49 +0100

    Java 8 -> Java 10, Task class added

--- src/main/java/com/colomertrave/microservices/microprofile/rest/HelloWorldEndpoint.java
+++ src/main/java/com/colomertrave/microservices/microprofile/rest/HelloWorldEndpoint.java
@@ -5,14 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Path("/hello")
+@Path("/task")
 public class HelloWorldEndpoint {
 
 	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Hello from Thorntail!").build();
+	@Produces("application/json")
+	public List<Task> doGet() {
+		Task t1 = new Task(1, "Learning microprofile");
+		final List<Task> lista = List.of(t1);
+		return lista;
 	}
 }
---
+++ src/main/java/com/colomertrave/microservices/microprofile/rest/Task.java
@@ -0,0 +1,31 @@
+package com.colomertrave.microservices.microprofile.rest;
+
+public class Task {
+    private int id;
+    private String description;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return "ID: " + id + " DESC: " + description;
+    }
+}
